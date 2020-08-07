4e52bc6f9f229664de455f2ef4bd31fb7b578239
CathyZhou0120 <CathyZhou@github.com>
2020-08-07 14:10:53 -0700

    Create connect_test.py

---
+++ connect_test.py
@@ -0,0 +1,27 @@
+import os
+from connect import AMLInterface
+
+
+def main():
+    storage_acct_name = os.environ['STORAGE_ACCT_NAME']
+    storage_acct_key = os.environ['STORAGE_ACCT_KEY']
+    workspace_name = os.environ['AML_WORKSPACE_NAME']
+    resource_group = os.environ['RESOURCE_GROUP']
+    subscription_id = os.environ['SUBSCRIPTION_ID']
+
+    spn_credentials = {
+        'tenant_id': os.environ['TENANT_ID'],
+        'service_principal_id': os.environ['SPN_ID'],
+        'service_principal_password': os.environ['SPN_PASSWORD'],
+    }
+    # Instantiate Blob Storage Interface
+    blob_storage_interface = BlobStorageInterface(
+        storage_acct_name, storage_acct_key
+    )
+
+    aml_interface = AMLInterface(
+        spn_credentials, subscription_id, workspace_name, resource_group
+    )
+    
+if __name__ == '__main__':
+    main()
