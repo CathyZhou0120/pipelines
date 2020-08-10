d7400aa7501bd58c295aa208cff93898b6f54b63
cathyzhou0120 <CathyZhou@github.com>
2020-08-09 18:54:45 -0700

    update

--- pull_from_psql.py
+++ pull_from_psql.py
@@ -6,8 +6,15 @@ import os
 # Construct connection string
 
 
-def get_data(conn_string):
-    conn = psycopg2.connect(conn_string) 
+def get_data(host,user,dbname,password,port,sslmode):
+    conn = psycopg2.connect(
+        host=host,
+    database=dbname,
+    user=user,
+    password=password,
+    port=port,
+    sslmode=sslmode
+    ) 
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM iris;")
     rows = cursor.fetchall()
@@ -29,9 +36,9 @@ def main():
     sslmode=os.environ['sslmode']
     tenent_id=os.environ['TENANT_ID']
 
-    conn_string="""host={0} user={1} dbname={2} password={3} port={4} sslmode={5}""".format(host, user, dbname, password, port, sslmode)
-    #get_data(conn_string)
-    print(conn_string, tenent_id)
+    #conn_string="""host={0} user={1} dbname={2} password={3} port={4} sslmode={5}""".format(host, user, dbname, password, port, sslmode)
+    get_data(host,user,dbname,password,port,sslmode)
+    #print(conn_string, tenent_id)
 
 
 if __name__ == '__main__':
