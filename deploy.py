ed81194adc0f3e4cc25d617b08f1d9c3ad351c8a
cathyzhou0120 <CathyZhou@github.com>
2020-08-12 18:52:40 -0700

    update

--- deploy.py
+++ deploy.py
@@ -12,50 +12,25 @@ from const import (
 __here__ = os.path.dirname(__file__)
 
 
-def get_inference_config(aml_interface):
-    aml_env = Environment.get(
-        workspace=aml_interface.workspace,
-        name=AML_ENV_NAME
-    )
-    scoring_script_path = os.path.join(__here__, 'score.py')
-    inference_config = InferenceConfig(
-        entry_script=scoring_script_path,
-        environment=aml_env
-    )
-    return inference_config
-
-
-def deploy_service(aml_interface):
-    inference_config = get_inference_config(aml_interface)
-    deployment_config = AciWebservice.deploy_configuration(
+def deploy(aml_interface,inference_config,service_name):
+    inference_config = inference_config
+    aci_config = AciWebservice.deploy_configuration(
         cpu_cores=1,
         memory_gb=1
     )
     model = aml_interface.workspace.models.get(MODEL_NAME)
+
     service = Model.deploy(
         aml_interface.workspace,
-        DEPLOYMENT_SERVICE_NAME,
-        [model],
-        inference_config,
-        deployment_config)
+        name=service_name,
+        models=[model],
+        inference_config=inference_config,
+        deployment_config=aci_config)
     service.wait_for_deployment(show_output=True)
     print(service.scoring_uri)
 
 
-def update_service(aml_interface):
-    inference_config = get_inference_config(aml_interface)
-    service = Webservice(
-        name=DEPLOYMENT_SERVICE_NAME,
-        workspace=aml_interface.workspace
-    )
-    model = aml_interface.workspace.models.get(MODEL_NAME)
-    service.update(models=[model], inference_config=inference_config)
-    print(service.state)
-    print(service.scoring_uri)
-
-
-def main():
-    # Retrieve vars from env
+def main(inference_config,service_name):
     workspace_name = os.environ['AML_WORKSPACE_NAME']
     resource_group = os.environ['RESOURCE_GROUP']
     subscription_id = os.environ['SUBSCRIPTION_ID']
@@ -69,12 +44,21 @@ def main():
     aml_interface = AMLInterface(
         spn_credentials, subscription_id, workspace_name, resource_group
     )
-    webservices = aml_interface.workspace.webservices.keys()
-    if DEPLOYMENT_SERVICE_NAME not in webservices:
-        deploy_service(aml_interface)
-    else:
-        update_service(aml_interface)
+    #webservices = aml_interface.workspace.webservices.keys()
+    
+    deploy(aml_interface,inference_config,service_name)
+
 
 
 if __name__ == '__main__':
-    main()
+
+    service_name = 'aml-pipeline-deploy'
+
+    scoring_script_path = os.path.join(__here__, 'score.py')
+    aml_env = Environment.get(
+        workspace=aml_interface.workspace,
+        name=AML_ENV_NAME
+    )
+    inference_config = InferenceConfig(entry_script=scoring_script_path, environment=aml_env)
+
+    main(inference_config,service_name)
