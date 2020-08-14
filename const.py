4823ca5868d7371f7d2f3f395bd7881e44ebe17d
cathyzhou0120 <CathyZhou@github.com>
2020-08-13 18:45:09 -0700

    update

--- const.py
+++ const.py
@@ -9,3 +9,7 @@ AML_EXPERIMENT_NAME = 'pipeline-experiment-3'
 
 MODEL_NAME = 'model.pkl'
 MODEL_VERSION = 5
+PREDICTION_FILE = 'prediction/data.csv'
+PREDICTION_PATH = 'prediction/'
+TARGET_PATH = 'prediction/'
+PREDICTION_DATASET_NAME= 'prediction_dataset'
---
+++ get_prediction_data.py
@@ -0,0 +1,58 @@
+import psycopg2
+import csv
+import os
+
+from azureml.core.dataset import Dataset
+from azureml.core.datastore import Datastore
+from connect import AMLInterface 
+from const import PREDICTION_FILE, PREDICTION_PATH, TARGET_PATH, PREDICTION_DATASET_NAME
+
+
+def get_data(host,user,dbname,password,port,sslmode):
+    conn = psycopg2.connect(
+        host=host,
+    database=dbname,
+    user=user,
+    password=password,
+    port=port,
+    sslmode=sslmode
+    ) 
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM iris LIMIT 20;")
+    rows = cursor.fetchall()
+
+    with open(PREDICTION_FILE, 'w') as f:
+        fieldnames = ['sepal_length', 'sepal_width','peta_length','petal_width','class']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in rows:
+            writer.writerow({'sepal_length': i[0], 'sepal_width': i[1],'peta_length': i[2],'petal_width': i[3],'class': i[4]})
+
+
+def register_dataset(aml_interface):
+    datastore = AMLInterface.workspace.get_default_datastore()
+    datastore.upload(src_dir=PREDICTION_PATH, target_path=TARGET_PATH)   
+
+    dataset = Dataset.Tabular.from_delimited_files(datastore.path(PREDICTION_FILE))
+    dataset = dataset.register(workspace=AMLInterface.workspace,
+                                 name=PREDICTION_DATASET_NAME)
+
+
+def main():
+    workspace_name = os.environ['AML_WORKSPACE_NAME']
+    resource_group = os.environ['RESOURCE_GROUP']
+    subscription_id = os.environ['SUBSCRIPTION_ID']
+
+    spn_credentials = {
+        'tenant_id': os.environ['TENANT_ID'],
+        'service_principal_id': os.environ['SPN_ID'],
+        'service_principal_password': os.environ['SPN_PASSWORD'],
+    }
+
+    aml_interface = AMLInterface(
+        spn_credentials, subscription_id, workspace_name, resource_group
+    )
+    register_dataset(aml_interface)
+
+if __name__ == '__main__':
+    main()
