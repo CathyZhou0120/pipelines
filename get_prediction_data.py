13219de26a270e7d5a1707453930e96d49647578
cathyzhou0120 <CathyZhou@github.com>
2020-08-20 08:51:23 -0700

    update

--- get_prediction_data.py
+++ get_prediction_data.py
@@ -56,7 +56,9 @@ def upload_data(blob_storage):
     upload_prediction_to_container(blob_storage)
 
 def register_dataset(datastore, path,aml_interface):
-    datastore_path = [(datastore, path)]
+    workspace= aml_interface.workspace
+    prediction_datastore = Datastore.get(workspace, datastore)
+    datastore_path = [(prediction_datastore, path)]
     dataset = Dataset.Tabular.from_delimited_files(
         path=datastore_path
     )
